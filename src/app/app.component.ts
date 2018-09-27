aed9aecb8f17a17ab2e076aa1d53d2e910f64263
mongooseasd <andras.more@saxsys.de>
2018-09-27 14:08:44 +0200

    Basic logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  articles: Article[];
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
+    return false;
+  }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
+  constructor() {
+    this.articles = [
+      new Article('Aaaaaangular', 'https://angular.io', 10),
+      new Article('Fuuullllstck', 'https://fullstack.io', 2),
+      new Article('Csip', 'https://csip.com', 2),
+    ];
+
+  }
+
 }
